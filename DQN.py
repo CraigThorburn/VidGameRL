a69e0a76591b95c742a1d1394feb13d8ea354a3f
craig <craigtho@umd.edu>
2021-03-04 17:28:23 -0500

    Added handling for loaded model

--- DQN.py
+++ DQN.py
@@ -147,6 +147,80 @@ class DQN_NN_conv(nn.Module):
 
 
 
+class DQN_NN_conv_pretrain(nn.Module):
+
+    def __init__(self, h, w, inputs, outputs, kernel = 5, sstride = 2, layers = [16, 32, 32, 20], freeze_convolution=False, n_phone_layer=39):
+        super(DQN_NN_conv_pretrain, self).__init__()
+        self.conv1channels, self.conv2channels, self.conv3channels, self.mid_size = layers
+
+
+        self.conv1 = nn.Conv2d(1, self.conv1channels, kernel_size=kernel, stride=sstride)
+        self.bn1 = nn.BatchNorm2d(self.conv1channels)
+        self.conv2 = nn.Conv2d(self.conv1channels, self.conv2channels, kernel_size=kernel, stride=sstride)
+        self.bn2 = nn.BatchNorm2d(self.conv2channels)
+        self.conv3 = nn.Conv2d(self.conv2channels, self.conv3channels, kernel_size=kernel, stride=sstride)
+        self.bn3 = nn.BatchNorm2d(self.conv3channels)
+
+        if freeze_convolution:
+            self.conv1.bias.requires_grad=False
+            self.conv1.weight.requires_grad = False
+            self.bn1.bias.requires_grad=False
+            self.bn1.weight.requires_grad=False
+            self.conv2.bias.requires_grad = False
+            self.conv2.weight.requires_grad = False
+            self.bn2.bias.requires_grad=False
+            self.bn2.weight.requires_grad=False
+            self.conv3.bias.requires_grad = False
+            self.conv3.weight.requires_grad = False
+            self.bn3.bias.requires_grad=False
+            self.bn3.weight.requires_grad=False
+
+        # Number of Linear input connections depends on output of conv2d layers
+        # and therefore the input image size, so compute it.
+        def conv2d_size_out(size, kernel_size = kernel, stride = sstride):
+            return (size - (kernel_size - 1) - 1) // stride  + 1
+
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
+        self.linear_input_size = convw * convh * self.conv3channels
+
+        self.lin1 = nn.Linear(self.linear_input_size, n_phone_layer)
+
+        self.head1 = nn.Linear(n_phone_layer + inputs,self.mid_size)
+        self.head2 = nn.Linear(self.mid_size, outputs)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x_aud, x_loc):
+
+        if len(x_aud.size()) == 2:
+            # not batch
+            x_aud = x_aud.reshape(1,1,x_aud.size()[0], x_aud.size()[1])
+            x_aud = F.relu(self.bn1(self.conv1(x_aud)))
+            x_aud = F.relu(self.bn2(self.conv2(x_aud)))
+            x_aud = F.relu(self.bn2(self.conv3(x_aud)))
+
+            x = x.reshape(1, x_aud.size()[1] * x_aud.size()[2] * x_aud.size()[3])
+            x_aud = F.relu(self.lin1(x_aud))
+            x = torch.cat((x_aud, x_loc))
+        else:
+            # batch
+            x_aud = x_aud.reshape(x_aud.size()[0],1,x_aud.size()[1], x_aud.size()[2])
+            x_aud = F.relu(self.bn1(self.conv1(x_aud)))
+            x_aud = F.relu(self.bn2(self.conv2(x_aud)))
+            x_aud = F.relu(self.bn2(self.conv3(x_aud)))
+
+            x_aud = x_aud.reshape(x_aud.size()[0], x_aud.size()[1] * x_aud.size()[2] * x_aud.size()[3])
+            x_aud = F.relu(self.lin1(x_aud))
+            x = torch.cat((x_aud , x_loc), 1)
+
+        x = F.softplus(self.head1(x))
+        x = F.softplus(self.head2(x))
+
+
+        return x
+
+
 class DQN_convLSTM(nn.Module):
 
     def __init__(self, h, w, inputs, outputs, kernel = 5, sstride = 2):
