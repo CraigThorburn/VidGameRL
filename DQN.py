Fix conv scaling and add connection parameter

DQN.py
@@ -147,7 +147,7 @@ class DQN_NN_conv(nn.Module):
 
 
 
-class DQN_NN_conv_pretrain(nn.Module):
+class DQN_NN_conv_pretrain_phonelayer(nn.Module):
 
     def __init__(self, h, w, inputs, outputs, kernel = 5, sstride = 2, layers = [16, 32, 32, 20], freeze_convolution=False, n_phone_layer=39, freeze_layer = 0):
         super(DQN_NN_conv_pretrain, self).__init__()
@@ -177,22 +177,22 @@ class DQN_NN_conv_pretrain(nn.Module):
         self.head2 = nn.Linear(self.mid_size, outputs)
 
         if freeze_convolution:
-            if freeze_layer <= -4:
+            if freeze_layer >= 1:
                 self.conv1.bias.requires_grad=False
                 self.conv1.weight.requires_grad = False
                 self.bn1.bias.requires_grad=False
                 self.bn1.weight.requires_grad=False
-            if freeze_layer <= -3:
+            if freeze_layer >= 2:
                 self.conv2.bias.requires_grad = False
                 self.conv2.weight.requires_grad = False
                 self.bn2.bias.requires_grad=False
                 self.bn2.weight.requires_grad=False
-            if freeze_layer <= -2:
+            if freeze_layer >= 3:
                 self.conv3.bias.requires_grad = False
                 self.conv3.weight.requires_grad = False
                 self.bn3.bias.requires_grad=False
                 self.bn3.weight.requires_grad=False
-            if freeze_layer <= -1:
+            if freeze_layer >= 4:
                 self.lin1.weight.requires_grad=False
                 self.lin1.bias.requires_grad=False
 
@@ -216,35 +216,6 @@ class DQN_NN_conv_pretrain(nn.Module):
 
         return x
 
-    # noinspection DuplicatedCode,PyUnreachableCode
-    def forward_old(self, x_aud, x_loc):
-        raise NotImplementedError
-        if len(x_aud.size()) == 2:
-            # not batch
-            x_aud = x_aud.reshape(1,1,x_aud.size()[0], x_aud.size()[1])
-            x_aud = F.relu(self.bn1(self.conv1(x_aud)))
-            x_aud = F.relu(self.bn2(self.conv2(x_aud)))
-            x_aud = F.relu(self.bn2(self.conv3(x_aud)))
-
-            x_aud = x_aud.reshape(x_aud.size()[1] * x_aud.size()[2] * x_aud.size()[3])
-            x_aud = F.softplus(self.lin1(x_aud))
-            x = torch.cat((x_aud, x_loc))
-        else:
-            # batch
-            x_aud = x_aud.reshape(x_aud.size()[0],1,x_aud.size()[1], x_aud.size()[2])
-            x_aud = F.relu(self.bn1(self.conv1(x_aud)))
-            x_aud = F.relu(self.bn2(self.conv2(x_aud)))
-            x_aud = F.relu(self.bn2(self.conv3(x_aud)))
-
-            x_aud = x_aud.reshape(x_aud.size()[0], x_aud.size()[1] * x_aud.size()[2] * x_aud.size()[3])
-            x_aud = F.softplus(self.lin1(x_aud))
-            x = torch.cat((x_aud , x_loc), 1)
-
-        x = F.softplus(self.head1(x))
-        x = F.softplus(self.head2(x))
-
-
-        return x
 
     def phone_class(self, x, relu):
 
@@ -259,32 +230,69 @@ class DQN_NN_conv_pretrain(nn.Module):
 
         return x
 
-        # if len(x_aud.size()) == 2:
-        #     # not batch
-        #     #x_aud = x_aud.reshape(1,1,x_aud.size()[0], x_aud.size()[1])
-        #     x_aud = F.relu(self.bn1(self.conv1(x_aud)))
-        #     x_aud = F.relu(self.bn2(self.conv2(x_aud)))
-        #     x_aud = F.relu(self.bn2(self.conv3(x_aud)))
-        #
-        #     x_aud = x_aud.reshape(x_aud.size()[1] * x_aud.size()[2] * x_aud.size()[3])
-        #     if relu:
-        #         x_aud = F.relu(self.lin1(x_aud))
-        #     else:
-        #         x_aud = F.softplus(self.lin1(x_aud))
-        # else:
-        #     # batch
-        #     #x_aud = x_aud.reshape(x_aud.size()[0],1,x_aud.size()[1], x_aud.size()[2])
-        #     x_aud = F.relu(self.bn1(self.conv1(x_aud)))
-        #     x_aud = F.relu(self.bn2(self.conv2(x_aud)))
-        #     x_aud = F.relu(self.bn2(self.conv3(x_aud)))
-        #
-        #     x_aud = x_aud.reshape(x_aud.size()[0], x_aud.size()[1] * x_aud.size()[2] * x_aud.size()[3])
-        #     if relu:
-        #         x_aud = F.relu(self.lin1(x_aud))
-        #     else:
-        #         x_aud = F.softplus(self.lin1(x_aud))
-        #
-        # return x_aud
+class DQN_NN_conv_pretrain_convlayer(nn.Module):
+
+    def __init__(self, h, w, inputs, outputs, kernel = 5, sstride = 2, layers = [16, 32, 32, 20], freeze_convolution=False, n_phone_layer=39, freeze_layer = 0):
+        super(DQN_NN_conv_pretrain, self).__init__()
+        self.conv1channels, self.conv2channels, self.conv3channels, self.mid_size = layers
+
+
+        self.conv1 = nn.Conv2d(1, self.conv1channels, kernel_size=kernel, stride=sstride)
+        self.bn1 = nn.BatchNorm2d(self.conv1channels)
+        self.conv2 = nn.Conv2d(self.conv1channels, self.conv2channels, kernel_size=kernel, stride=sstride)
+        self.bn2 = nn.BatchNorm2d(self.conv2channels)
+        self.conv3 = nn.Conv2d(self.conv2channels, self.conv3channels, kernel_size=kernel, stride=sstride)
+        self.bn3 = nn.BatchNorm2d(self.conv3channels)
+
+
+        # Number of Linear input connections depends on output of conv2d layers
+        # and therefore the input image size, so compute it.
+        def conv2d_size_out(size, kernel_size = kernel, stride = sstride):
+            return (size - (kernel_size - 1) - 1) // stride  + 1
+
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
+        self.linear_input_size = convw * convh * self.conv3channels
+
+        self.head1 = nn.Linear(self.linear_input_size + inputs,self.mid_size)
+        self.head2 = nn.Linear(self.mid_size, outputs)
+
+        if freeze_convolution:
+            if freeze_layer >= 1:
+                self.conv1.bias.requires_grad=False
+                self.conv1.weight.requires_grad = False
+                self.bn1.bias.requires_grad=False
+                self.bn1.weight.requires_grad=False
+            if freeze_layer >= 2:
+                self.conv2.bias.requires_grad = False
+                self.conv2.weight.requires_grad = False
+                self.bn2.bias.requires_grad=False
+                self.bn2.weight.requires_grad=False
+            if freeze_layer >= 3:
+                self.conv3.bias.requires_grad = False
+                self.conv3.weight.requires_grad = False
+                self.bn3.bias.requires_grad=False
+                self.bn3.weight.requires_grad=False
+
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x_aud, x_loc):
+
+        x_aud = F.relu(self.bn1(self.conv1(x_aud)))
+        x_aud = F.relu(self.bn2(self.conv2(x_aud)))
+        x_aud = F.relu(self.bn3(self.conv3(x_aud)))
+
+        x_aud = x_aud.reshape(x_aud.size()[0], x_aud.size()[1] * x_aud.size()[2] * x_aud.size()[3])
+
+        x = torch.cat((x_aud, x_loc), 1)
+
+        x = F.softplus(self.head1(x))
+        x = F.softplus(self.head2(x))
+
+
+        return x
+
 
 
 

main_game_from_pretrain.py
@@ -172,8 +172,14 @@ print('num actions: ' + str(n_actions))
 w,h = env.get_aud_dims()
 
 ### Create Model Networks
-policy_net = DQN_NN_conv_pretrain(h, w,num_inputs, n_actions, KERNEL, STRIDE, LAYERS, CONV_FREEZE, n_phone_layer = NUM_PHONES, freeze_layer=CONV_FREEZE_LAYER).to(device)
-target_net = DQN_NN_conv_pretrain(h, w, num_inputs, n_actions, KERNEL, STRIDE, LAYERS, CONV_FREEZE, n_phone_layer = NUM_PHONES, freeze_layer=CONV_FREEZE_LAYER).to(device)
+if CONNECTION_LAYER == 'phone':
+    policy_net = DQN_NN_conv_pretrain_phonelayer(h, w,num_inputs, n_actions, KERNEL, STRIDE, LAYERS, CONV_FREEZE, n_phone_layer = NUM_PHONES, freeze_layer=CONV_FREEZE_LAYER).to(device)
+    target_net = DQN_NN_conv_pretrain_phonelayer(h, w, num_inputs, n_actions, KERNEL, STRIDE, LAYERS, CONV_FREEZE, n_phone_layer = NUM_PHONES, freeze_layer=CONV_FREEZE_LAYER).to(device)
+elif CONNECTION_LAYER == 'conv':
+    policy_net = DQN_NN_conv_pretrain_convlayer(h, w,num_inputs, n_actions, KERNEL, STRIDE, LAYERS, CONV_FREEZE, n_phone_layer = NUM_PHONES, freeze_layer=CONV_FREEZE_LAYER).to(device)
+    target_net = DQN_NN_conv_pretrain_convlayer(h, w, num_inputs, n_actions, KERNEL, STRIDE, LAYERS, CONV_FREEZE, n_phone_layer = NUM_PHONES, freeze_layer=CONV_FREEZE_LAYER).to(device)
+else:
+    raise NotImplementedError
 
 MODEL_LOCATION= ROOT + MODEL_FOLDER + 'model_' + PRETRAIN_MODELNAME + '_final.pt'
 policy_net.load_state_dict(torch.load(MODEL_LOCATION, map_location=device), strict=False)