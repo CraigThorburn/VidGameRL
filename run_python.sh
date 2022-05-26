2a6d9df6af82e46233a6d00f3de180b57626cdac
Craig Thorburn <craigtho@umiacs.umd.edu>
2022-05-26 17:52:05 -0400

    changes allow for stage 10 which runs h5features generation

--- run_python.sh
+++ run_python.sh
@@ -8,6 +8,6 @@ source activate audneurorl
 module add cuda
 
 echo "---------------------"
-echo "launching "$function" with parameters "$params" run number"$i
+echo "launching "$function" with parameters "$params" run number "$i
 python $function $params -run_num=$i || exit
 echo "finished"
